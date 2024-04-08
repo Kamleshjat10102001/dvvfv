758e89be7b107cef26c6871a64d9538374eb6642
noobdev777 <164876911+noobdev777@users.noreply.github.com>
2024-04-08 08:26:50 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "enter api id"
-api_hash = "enter api hash"
-bot_token = "bottoken"
+api_id = "27665762"
+api_hash = "b2b6f18e1280f194b8f7349db4737eb9"
+bot_token = "6824713244:AAGYH9wzLXiIEQRXwWmZn9pZUNKFwWYydoY"
