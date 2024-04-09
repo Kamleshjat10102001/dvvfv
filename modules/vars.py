5eae9463f9768ca2cd5b7188fc5e0cd1a9f65fcf
Kamleshjat10102001 <162905080+Kamleshjat10102001@users.noreply.github.com>
2024-04-09 06:53:05 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27665762"
-api_hash = "b2b6f18e1280f194b8f7349db4737eb9"
-bot_token = "6824713244:AAGYH9wzLXiIEQRXwWmZn9pZUNKFwWYydoY"
+api_id = "22334122"
+api_hash = "ab8de5c057fffb8387d935ec2c07f891"
+bot_token = "7154458821:AAFQ2r_IADruk4r6dtz2fGZ4wgXowEYGXpA"
